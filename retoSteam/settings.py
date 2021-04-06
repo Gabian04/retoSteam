06465a10080a0e68551b5e2c93c5628feee0964e
Gabian04 <65411496+Gabian04@users.noreply.github.com>
2021-04-06 16:02:07 -0500

    Update settings.py

--- retoSteam/settings.py
+++ retoSteam/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = '1qn^x&8*^6ltjex&(12##omsc-)&7(z%9e!v*8sw(t4m%!ep=n'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
